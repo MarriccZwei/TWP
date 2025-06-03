8acb0de3a488062e7356912e25f73f315bdf6c18
MarriccZwei <marricc7@gmail.com>
2025-06-03 15:35:58 +0200

    enclosed

--- SizingLoop/componentClasses.py
+++ SizingLoop/componentClasses.py
@@ -14,15 +14,17 @@ class Component(abc.ABC):
         '''returns an element connected to the specfied nodes, with properties corresponding of the global elements of the component'''
         raise NotImplementedError
     
+    @property
     @abc.abstractmethod
-    def net(self, settings:mst.Meshsettings)->ty.List[gcl.Point3D]:
-        '''returns a net of nodes on an element compliant with mesh settings'''
+    def net(self):
+        '''returns the mesh on the element'''
         raise NotImplementedError
+
 
 # For now, we ommit the spar, just to test everything and keep it simple   
 class StiffenedRib(Component):
     '''The diagonal rib, bent at the ends, supported with stiffeners'''
-    def __init__(self, jointNear:arp.JointPoints, jointFar:arp.JointPoints, stiffTowardsNear=True):
+    def __init__(self, jointNear:arp.JointPoints, jointFar:arp.JointPoints, settings:mst.Meshsettings, stiffTowardsNear=True):
         #super().__init__() #for now we don't have any parent variables
         #adjusting joints for the connection in the middle
         self.jointNear = arp.JointPoints(jointNear.fi.pt_between(jointNear.fo, .5),
@@ -32,9 +34,13 @@ class StiffenedRib(Component):
                                         jointFar.fo.pt_between(jointFar.fi, .5),
                                         jointFar.ro.pt_between(jointFar.ri, .5))
         self.stiffTowardsNear=stiffTowardsNear #direction of stiffeners - towards near direction of far direction
+        self.__net =self. _net(settings)
 
+    @property
+    def net(self):
+        return self.__net
 
-    def net(self, settings:mst.Meshsettings)->ty.List[gcl.Point3D]:
+    def _net(self, settings:mst.Meshsettings)->ty.List[gcl.Point3D]:
         nearm = gcl.quad_mesh(self.jointNear.fi, self.jointNear.fo, self.jointNear.ro, self.jointNear.ri,
                               settings.nbf, settings.nc)
         dm = gcl.quad_mesh(self.jointNear.fo, self.jointFar.fi, self.jointFar.ri, self.jointNear.ro,
@@ -57,8 +63,8 @@ if __name__ == "__main__":
     testJointFar = arp.JointPoints(gcl.Point3D(0.3, 4, 1), gcl.Point3D(1.6, 4, 1), 
                                     gcl.Point3D(0.34, 4+jointWidthTest*np.cos(top_angle), 1+jointWidthTest*np.sin(top_angle)),
                                     gcl.Point3D(1.57, 4+jointWidthTest*np.cos(top_angle), 1+jointWidthTest*np.sin(top_angle)))
-    testSrib = StiffenedRib(testJointNear, testJointFar, True)
-    testSribNet = testSrib.net(sets)
+    testSrib = StiffenedRib(testJointNear, testJointFar, sets, True)
+    testSribNet = testSrib.net
 
     from mpl_toolkits import mplot3d
     import matplotlib.pyplot as plt
