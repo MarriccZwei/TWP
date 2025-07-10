7df246ac0d63edf7892e11da1f1401f6442a2b6d
MarriccZwei <marricc7@gmail.com>
2025-07-10 23:05:02 +0200

    Update pyfe3Dgcl.py

--- C3/pyfe3Dgcl.py
+++ C3/pyfe3Dgcl.py
@@ -51,6 +51,20 @@ def eles_from_gcl(mesh:gcl.Mesh3D, eleDict:ty.Dict[str, ty.Dict[str, object]]):
         quad.update_KC0(KC0r, KC0c, KC0v, shellprop) #matrix contribution, changing the matrices sent
         created_eles["quad"].append(quad)
         init_k_KC0 += data["quad"].KC0_SPARSE_SIZE
+    #Spring elements
+    for conn in mesh.connections["spring"]:
+        spring = pf3.Spring(probes["spring"])
+        spring.n1 = nids[conn.ids[0]]
+        spring.n2 = nids[conn.ids[1]]
+        spring.c1 = pf3.DOF*nid_pos[spring.n1]
+        spring.c2 = pf3.DOF*nid_pos[spring.n2]
+        spring.kxe, spring.kye, spring.kze, spring.krxe, spring.krye, spring.krze = eleDict["spring"][conn.eleid]
+        spring.init_k_KC0 = init_k_KC0
+        spring.update_rotation_matrix(0, 1, 0, 1, 1, 0)
+        spring.update_KC0(KC0r, KC0c, KC0v)
+        created_eles["spring"].append(spring)
+        init_k_KC0 += data["spring"].KC0_SPARSE_SIZE
+
 
     #TODO: Other eles
 
@@ -61,13 +75,22 @@ def eles_from_gcl(mesh:gcl.Mesh3D, eleDict:ty.Dict[str, ty.Dict[str, object]]):
 
 if __name__ == "__main__":
     pts1 = [gcl.Point3D(-5, 0, 0), gcl.Point3D(-2.5, 10, 0)]
+    ptsMid = [gcl.Point3D(0,0,0), gcl.Point3D(0, 10, 0)]
     pts2 = [gcl.Point3D(5, 0, 0), gcl.Point3D(2.5, 10, 0)]
-    coords = gcl.multi_section_sheet3D(pts1, pts2, 11, [21])
+    coords1 = gcl.multi_section_sheet3D(pts1, ptsMid, 5, [21])
+    coords2 = gcl.multi_section_sheet3D(ptsMid, pts2, 6, [21])
 
     mesh = gcl.Mesh3D()
-    idspace = mesh.register(list(coords.flatten()))
-    idgrid = idspace.reshape(coords.shape)
-    mesh.quad_interconnect(idgrid, "quad1")
+    #might be worth automating
+    idspace1 = mesh.register(list(coords1.flatten()))
+    idgrid1 = idspace1.reshape(coords1.shape)
+    mesh.quad_interconnect(idgrid1, "quad1")
+    idspace2 = mesh.register(list(coords2.flatten()))
+    idgrid2 = idspace2.reshape(coords2.shape)
+    mesh.quad_interconnect(idgrid2, "quad1")
+
+    #springs at the boundary
+    mesh.spring_connect(idgrid1[-1, :], idgrid2[0, :], "spring1")
 
     #checking the mesh
     import matplotlib.pyplot as plt
@@ -76,6 +99,10 @@ if __name__ == "__main__":
         pts = [mesh.nodes[i] for i in conn.ids]
         x_, y_, _ = gcl.pts2coords3D(pts)
         plt.plot(x_, y_)
+    for conn in mesh.connections["spring"]:
+        pts = [mesh.nodes[i] for i in conn.ids]
+        x_, y_, _ = gcl.pts2coords3D(pts)
+        plt.scatter(x_, y_)
 
     #standard test data for the elements
     h = 0.005 # m
@@ -84,7 +111,8 @@ if __name__ == "__main__":
 
     #dictionaries for the element types
     import pyfe3d.shellprop_utils as psu
-    ele_prop = {'quad':{'quad1':psu.isotropic_plate(thickness=h, E=E, nu=nu, calc_scf=True)}}
+    ele_prop = {'quad':{'quad1':psu.isotropic_plate(thickness=h, E=E, nu=nu, calc_scf=True)},
+                'spring':{'spring1':(1e2, 1e2, 1e2, 1e2, 1e2, 1e2)}}
 
     #exporting mesh to pyfe3D
     KC0, N, x, y, z, _ = eles_from_gcl(mesh, ele_prop)
@@ -113,7 +141,7 @@ if __name__ == "__main__":
     
     KC0uu = KC0[bu, :][:, bu]
     fu = f[bu]
-    assert fu.sum() == fmid
+    assert fu.sum() == 2*fmid
 
     #@# actually solving the fem model
     from scipy.sparse.linalg import spsolve
