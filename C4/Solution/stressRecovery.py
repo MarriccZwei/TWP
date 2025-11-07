bd20a1aa92e09536929610293198950d6b3ea227
MarriccZwei <marricc7@gmail.com>
2025-11-06 22:24:03 -0800

    order was the problem

--- C4/Solution/stressRecovery.py
+++ C4/Solution/stressRecovery.py
@@ -1,176 +1,30 @@
 import pyfe3d as pf3
+import numpy as np 
 import pyfe3d.shellprop as psp
-import numpy as np
-import numpy.typing as nt
-import numpy.linalg as nl
 
-def strains_resultants_quad(probe:pf3.Quad4Probe, shellprop:psp.ShellProp):
+def strains_quad(probe:pf3.Quad4Probe):
     '''
-    The probe has to already be updated with the xe, ue and KC0ve of the quad!!!
+    Outputs the strains vector: [exx, eyy, gxy, kxx, kyy, kxy, gyz, gxz]
+    Obtained by constructing the BL matrix and multiplying it with the displacements vector
+    BLgxz, BLgyz are obtained by summing up the rot and grad BL row vectors before multiplication
+    BL constructed at xi, eta = (0,0), the element's integratio point
     '''
+    probe.update_BL(0,0)
+    return np.vstack([np.asarray(probe.BLexx),
+                      np.asarray(probe.BLeyy),
+                      np.asarray(probe.BLgxy),
+                      np.asarray(probe.BLkxx),
+                      np.asarray(probe.BLkyy),
+                      np.asarray(probe.BLkxy),
+                      np.asarray(probe.BLgyz_rot)+np.asarray(probe.BLgyz_grad),
+                      np.asarray(probe.BLgxz_rot)+np.asarray(probe.BLgxz_grad),])@np.asarray(probe.ue)
 
-    exx = list()
-    eyy = list()
-    gxy = list()
-    kxx = list()
-    kyy = list()
-    kxy = list()
-    gxz = list()
-    gyz = list()
-
-    Nxx = list()
-    Nyy = list()
-    Nxy = list()
-    Mxx = list()
-    Myy = list()
-    Mxy = list()
-    Qxz = list()
-    Qyz = list()
-
-    A11 = shellprop.A11
-    A12 = shellprop.A12
-    A22 = shellprop.A22
-    A16 = shellprop.A16
-    A26 = shellprop.A26
-    A66 = shellprop.A66
-
-    B11 = shellprop.B11
-    B12 = shellprop.B12
-    B22 = shellprop.B22
-    B16 = shellprop.B16
-    B26 = shellprop.B26
-    B66 = shellprop.B66
-
-    D11 = shellprop.D11
-    D12 = shellprop.D12
-    D22 = shellprop.D22
-    D16 = shellprop.D16
-    D26 = shellprop.D26
-    D66 = shellprop.D66
-
-    E44 = shellprop.E44
-    E45 = shellprop.E45
-    E55 = shellprop.E55
-
-    for xi, eta in [(-1,-1), (-1,1), (1,-1), (1,1)]:
-        probe.update_BL(xi, eta)
-        exx.append(np.dot(probe.BLexx, probe.ue))
-        eyy.append(np.dot(probe.BLeyy, probe.ue))
-        gxy.append(np.dot(probe.BLgxy, probe.ue))
-        kxx.append(np.dot(probe.BLkxx, probe.ue))
-        kyy.append(np.dot(probe.BLkyy, probe.ue))
-        kxy.append(np.dot(probe.BLkxy, probe.ue))
-        gxz_rot = np.dot(probe.BLgxz_rot, probe.ue)
-        gxz_grad = np.dot(probe.BLgxz_grad, probe.ue)
-        gxz.append(gxz_rot+gxz_grad)
-        gyz_rot = np.dot(probe.BLgyz_rot, probe.ue)
-        gyz_grad = np.dot(probe.BLgyz_grad, probe.ue)
-        gyz.append(gyz_rot+gyz_grad)
-
-        Nxx.append(A11*exx[-1]+A12*eyy[-1]+A16*gxy[-1]+B11*kxx[-1]+B12*kyy[-1]+B16*kxy[-1])
-        Nyy.append(A12*exx[-1]+A22*eyy[-1]+A26*gxy[-1]+B12*kxx[-1]+B22*kyy[-1]+B26*kxy[-1])
-        Nxy.append(A16*exx[-1]+A26*eyy[-1]+A66*gxy[-1]+B16*kxx[-1]+B26*kyy[-1]+B66*kxy[-1])
-        Mxx.append(D11*kxx[-1]+D12*kyy[-1]+D16*kxy[-1]+B11*exx[-1]+B12*eyy[-1]+B16*gxy[-1])
-        Myy.append(D12*kxx[-1]+D22*kyy[-1]+D26*kxy[-1]+B12*exx[-1]+B22*eyy[-1]+B26*gxy[-1])
-        Mxy.append(D16*kxx[-1]+D26*kyy[-1]+D66*kxy[-1]+B16*exx[-1]+B26*eyy[-1]+B66*gxy[-1])
-        Qxz.append(E44*gxz[-1]+E45*gyz[-1])
-        Qyz.append(E45*gxz[-1]+E55*gyz[-1])
-
-    # linear interpolation coefficients
-    lambdas = dict()
-
-    def interpol(strains):
-        N1 = eta*xi/4. - eta/4. - xi/4. + 1/4.
-        N2 = -eta*xi/4. - eta/4. + xi/4. + 1/4.
-        N3 = eta*xi/4. + eta/4. + xi/4. + 1/4.
-        N4 = -eta*xi/4. + eta/4. - xi/4. + 1/4.
-
-    Cexx = exx[0]
-    Aexx = exx[1]-Cexx
-    Bexx = exx[2]-Cexx
-
-    Ceyy = eyy[0]
-    Aeyy = eyy[1]-Ceyy
-    Beyy = eyy[2]-Ceyy
-
-    Cgxy = gxy[0]
-    Agxy = gxy[1]-Cgxy
-    Bgxy = gxy[2]-Cgxy
-
-    Ckxx = kxx[0]
-    Akxx = kxx[1]-Ckxx
-    Bkxx = kxx[2]-Ckxx
-
-    Ckyy = kyy[0]
-    Akyy = kyy[1]-Ckyy
-    Bkyy = kyy[2]-Ckyy
-
-    Ckxy = kxy[0]
-    Akxy = kxy[1]-Ckxy
-    Bkxy = kxy[2]-Ckxy
-
-    Cgxz = gxz[0]
-    Agxz = gxz[1]-Cgxz
-    Bgxz = gxz[2]-Cgxz
-
-    Cgyz = gyz[0]
-    Agyz = gyz[1]-Cgyz
-    Bgyz = gyz[2]-Cgyz
-
-    CNxx = Nxx[0]
-    ANxx = Nxx[1]-CNxx
-    BNxx = Nxx[2]-CNxx
-
-    CNyy = Nyy[0]
-    ANyy = Nyy[1]-CNyy
-    BNyy = Nyy[2]-CNyy
-
-    CNxy = Nxy[0]
-    ANxy = Nxy[1]-CNxy
-    BNxy = Nxy[2]-CNxy
-
-    CMxx = Mxx[0]
-    AMxx = Mxx[1]-CMxx
-    BMxx = Mxx[2]-CMxx
-
-    CMyy = Myy[0]
-    AMyy = Myy[1]-CMyy
-    BMyy = Myy[2]-CMyy
-
-    CMxy = Mxy[0]
-    AMxy = Mxy[1]-CMxy
-    BMxy = Mxy[2]-CMxy
-
-    CQxz = Qxz[0]
-    AQxz = Qxz[1]-CQxz
-    BQxz = Qxz[2]-CQxz
-
-    CQyz = Qyz[0]
-    AQyz = Qyz[1]-CQyz
-    BQyz = Qyz[2]-CQyz
-
-    return {
-        "exx":lambda xi, eta: Aexx*xi+Bexx*eta+Cexx,
-        "eyy":lambda xi, eta: Aeyy*xi+Beyy*eta+Ceyy,
-        "gxy":lambda xi, eta: Agxy*xi+Bgxy*eta+Cgxy,
-        "kxx":lambda xi, eta: Akxx*xi+Bkxx*eta+Ckxx,
-        "kyy":lambda xi, eta: Akyy*xi+Bkyy*eta+Ckyy,
-        "kxy":lambda xi, eta: Akxy*xi+Bkxy*eta+Ckxy,
-        "gxz":lambda xi, eta: Agxz*xi+Bgxz*eta+Cgxz,
-        "gyz":lambda xi, eta: Agyz*xi+Bgyz*eta+Cgyz,
-
-        "Nxx":lambda xi, eta: ANxx*xi+BNxx*eta+CNxx,
-        "Nyy":lambda xi, eta: ANyy*xi+BNyy*eta+CNyy,
-        "Nxy":lambda xi, eta: ANxy*xi+BNxy*eta+CNxy,
-        "Mxx":lambda xi, eta: AMxx*xi+BMxx*eta+CMxx,
-        "Myy":lambda xi, eta: AMyy*xi+BMyy*eta+CMyy,
-        "Mxy":lambda xi, eta: AMxy*xi+BMxy*eta+CMxy,
-        "Qxz":lambda xi, eta: AQxz*xi+BQxz*eta+CQxz,
-        "Qyz":lambda xi, eta: AQyz*xi+BQyz*eta+CQyz,
-    }
-
-
-    
-
-        
-        
+def get_ABDE(prop:psp.ShellProp):
+    return np.array([[prop.A11, prop.A12, prop.A16, prop.B11, prop.B12, prop.B16, 0, 0],
+                         [prop.A12, prop.A22, prop.A26, prop.B12, prop.B22, prop.B26, 0, 0],
+                         [prop.A16, prop.A26, prop.A66, prop.B16, prop.B26, prop.B66, 0, 0],
+                         [prop.B11, prop.B12, prop.B16, prop.D11, prop.D12, prop.D16, 0, 0],
+                         [prop.B12, prop.B22, prop.B26, prop.D12, prop.D22, prop.D26, 0, 0],
+                         [prop.B16, prop.B26, prop.B66, prop.D16, prop.D26, prop.D66, 0, 0],
+                         [0, 0, 0, 0, 0, 0, prop.E44, prop.E45],
+                         [0, 0, 0, 0, 0, 0, prop.E45, prop.E55]], dtype=pf3.DOUBLE)
--- test/Solution/test_stressRecovery.py
+++ test/Solution/test_stressRecovery.py
@@ -6,20 +6,21 @@ sys.path.append('..')
 import numpy as np
 from scipy.sparse.linalg import spsolve
 from scipy.sparse import coo_matrix
+import typing as ty
 
 from pyfe3d.shellprop_utils import isotropic_plate
 from pyfe3d import Quad4, Quad4Data, Quad4Probe, INT, DOUBLE, DOF
 
 
-def test_static_plate_quad_point_load_x(plot=False):
+def test_static_plate_quad_point_load(xdim:float, ydim:float, loads2apply:ty.List[int], plot=False):
     data = Quad4Data()
     probe = Quad4Probe()
-    nx = 26
-    ny = 11
+    nx = 11*(int(xdim/ydim/4)+1)
+    ny = 11*(int(ydim/xdim/4)+1)
 
-    a = 13
-    b = 3
-    h = 0.005 # m
+    a = xdim
+    b = ydim
+    h = 0.01 # m
 
     E = 200e9
     nu = 0.3
@@ -82,7 +83,7 @@ def test_static_plate_quad_point_load_x(plot=False):
     print('elements created')
 
     bk = np.zeros(N, dtype=bool)
-    check = np.isclose(x, 0.) #| np.isclose(x, a) | np.isclose(y, 0) | np.isclose(y, b)
+    check = np.isclose(x, 0.) if xdim>ydim else np.isclose(y, 0.)
     bk[0::DOF] = check
     bk[1::DOF] = check
     bk[2::DOF] = check
@@ -95,59 +96,74 @@ def test_static_plate_quad_point_load_x(plot=False):
     #load application
     fext = np.zeros(N)
     fmid = 1.
-    check1 = np.isclose(x, a)
-    check2 = np.isclose(x, a) & (np.isclose(y,b) | np.isclose(y,0))
 
-    def uniform_load(dof): 
-        fext[dof::DOF][check1] = fmid/(ny-1)
-        fext[dof::DOF][check2] /= 2
-    
-    # uniform tension, transverse shear
-    uniform_load(0)
-    uniform_load(2)
+    checkx = np.isclose(x, a)
+    checky = np.isclose(y, b)
 
-    #torque with transverse shear forces
+    def apply_load(check, idx, n):
+        fext[idx::pf3.DOF][check] = fmid/n
+
+    if 0 in loads2apply: #Nxx
+        apply_load(checkx, 0, ny)
+    if 1 in loads2apply: #Nyy
+        apply_load(checky, 1, nx)
+    if 2 in loads2apply: #Nxy
+        apply_load(checky, 0, nx)
+    if 3 in loads2apply: #Mxx
+        apply_load(checky, 3, nx)
+    if 4 in loads2apply: #Myy
+        apply_load(checkx, 4, ny)
+    if 5 in loads2apply: #Mxy
+        apply_load(checky, 4, nx)
+    if 7 in loads2apply: #Qxz
+        apply_load(checky, 2, nx)
+    if 6 in loads2apply: #Qyz
+        apply_load(checkx, 2, ny)
+
 
     KC0uu = KC0[bu, :][:, bu]
-    assert np.isclose(fext[bu].sum(), fmid*2)
+    assert np.isclose(fext[bu].sum(), fmid*len(loads2apply))
 
     uu = spsolve(KC0uu, fext[bu])
 
     u = np.zeros(N)
     u[bu] = uu
 
-    x2plot = np.zeros(num_elements)
-    y2plot = np.zeros(num_elements)
-    Nxx2plot = np.zeros(num_elements)
-    Nxy2plot = np.zeros(num_elements)
-    Myy2plot = np.zeros(num_elements)
-    Mxy2plot = np.zeros(num_elements)
-    Qyz2plot = np.zeros(num_elements)
+    exx, eyy, gxy, kxx, kyy, kxy, gyz, gxz, x2plot, y2plot = (np.zeros(num_elements), np.zeros(num_elements), 
+    np.zeros(num_elements), np.zeros(num_elements), np.zeros(num_elements), np.zeros(num_elements), np.zeros(num_elements),
+    np.zeros(num_elements), np.zeros(num_elements), np.zeros(num_elements))
+
+    Nxx, Nyy,Nxy, Mxx, Myy, Mxy, Qxz, Qyz = (np.zeros(num_elements), np.zeros(num_elements), 
+    np.zeros(num_elements), np.zeros(num_elements), np.zeros(num_elements), np.zeros(num_elements), np.zeros(num_elements),
+    np.zeros(num_elements))
     
+    ABDE = get_ABDE(prop)
+
     fint = np.zeros(N)
     for i, quad in enumerate(quads):
         quad.update_probe_xe(ncoords_flatten)
         quad.update_probe_ue(u)
         quad.update_fint(fint, prop)
-        interpols = strains_resultants_quad(probe, prop)
+        strains = strains_quad(probe)
+        resultants = ABDE@strains
+        exx[i] = strains[0]
+        eyy[i] = strains[1]
+        gxy[i] = strains[2]
+        kxx[i] = strains[3]
+        kyy[i] = strains[4]
+        kxy[i] = strains[5]
+        gxz[i] = strains[6]
+        gyz[i] = strains[7]
         x2plot[i] = x[nid_pos[quad.n1]]
-        y2plot[i] = y[nid_pos[quad.n1]]
-        Nxx2plot[i]= interpols["Nxx"](0, 0)
-        Myy2plot[i]= interpols["Myy"](0, 0)
-        Mxy2plot[i]= interpols["Mxy"](0, 0)
-        Qyz2plot[i]= interpols["Qyz"](1, -1)
-
-        if 2*b<x2plot[i]<a-1.5*b: #Saint-Venant principle
-            def assert_res_uniform(resultant, expr):
-                ResAround = np.array([interpols[resultant](xi, eta) for xi, eta in [(-1,-1), (1,-1), (-1, 1), (1,1), (0,0)]])
-                assert np.allclose(ResAround, expr, rtol = 1e-2), f"{ResAround}, gt: {expr}, @x {x2plot[i]}"
-            assert_res_uniform("Nxx", fmid/b)
-            #assert_res_uniform("Qyz", fmid/b)
-            #assert_res_uniform("Myy", -(a-x2plot[i])*fmid/b)
-
-        
-
-    print("Stress recovery tests passed.")
+        y2plot[i] = y[nid_pos[quad.n2]]
+        Nxx[i] = resultants[0]
+        Nyy[i] = resultants[1]
+        Nxy[i] = resultants[2]
+        Mxx[i] = resultants[3]
+        Myy[i] = resultants[4]
+        Mxy[i] = resultants[5]
+        Qxz[i] = resultants[6]
+        Qyz[i] = resultants[7]
 
     # NOTE adding reaction forces to external force vector
     Kku = KC0[bk, :][:, bu]
@@ -163,367 +179,39 @@ def test_static_plate_quad_point_load_x(plot=False):
     if plot:
         import matplotlib.pyplot as plt
 
-        ax = plt.subplot(2,2,1)
-        uz = u[2::DOF].reshape(nx,ny).T
-        levels = np.linspace(uz.min(), uz.max(), 20)
-        plt.contourf(xmesh, ymesh, uz, levels=levels)
-        plt.colorbar()
-        ax.set_title("u_z")
-
         def resultant_plot(i, qty, title):
-            ax = plt.subplot(2,2,i)
-            levels = np.linspace(qty.min(), qty.max(), 20)
+            ax = plt.subplot(2,4,i)
+            levels = np.linspace(qty.min(), qty.max(), 50)
             plt.contourf(x2plot.reshape((nx-1, ny-1)).T, 
                          y2plot.reshape((nx-1, ny-1)).T, 
                          qty.reshape((nx-1, ny-1)).T, levels=levels)
             plt.colorbar()
             ax.set_title(title)
 
-        resultant_plot(2, Nxx2plot, "Nxx")
-        resultant_plot(3, Qyz2plot, "Qyz")
-        resultant_plot(4, Myy2plot, "Myy")
+        resultant_plot(1, exx, "exx")
+        resultant_plot(2, eyy, "eyy")
+        resultant_plot(3, gxy, "gxy")
+        resultant_plot(4, gxz, "gxz")
+        resultant_plot(8, gyz, "gyz")
+        resultant_plot(5, kxx, "kxx")
+        resultant_plot(6, kyy, "kyy")
+        resultant_plot(7, kxy, "kxy")
 
         plt.show()
 
-def test_static_plate_quad_point_load_y(plot=False):
-    data = Quad4Data()
-    probe = Quad4Probe()
-    nx = 26
-    ny = 11
-
-    a = 3
-    b = 13
-    h = 0.005 # m
-
-    E = 200e9
-    nu = 0.3
-
-    xtmp = np.linspace(0, a, nx)
-    ytmp = np.linspace(0, b, ny)
-    xmesh, ymesh = np.meshgrid(xtmp, ytmp)
-    ncoords = np.vstack((xmesh.T.flatten(), ymesh.T.flatten(), np.zeros_like(ymesh.T.flatten()))).T
-    x = ncoords[:, 0]
-    y = ncoords[:, 1]
-    z = ncoords[:, 2]
-    ncoords_flatten = ncoords.flatten()
-
-    nids = 1 + np.arange(ncoords.shape[0])
-    nid_pos = dict(zip(nids, np.arange(len(nids))))
-    nids_mesh = nids.reshape(nx, ny)
-    n1s = nids_mesh[:-1, :-1].flatten()
-    n2s = nids_mesh[1:, :-1].flatten()
-    n3s = nids_mesh[1:, 1:].flatten()
-    n4s = nids_mesh[:-1, 1:].flatten()
-
-    num_elements = len(n1s)
-
-    KC0r = np.zeros(data.KC0_SPARSE_SIZE*num_elements, dtype=INT)
-    KC0c = np.zeros(data.KC0_SPARSE_SIZE*num_elements, dtype=INT)
-    KC0v = np.zeros(data.KC0_SPARSE_SIZE*num_elements, dtype=DOUBLE)
-    N = DOF*nx*ny
-
-    prop = isotropic_plate(thickness=h, E=E, nu=nu, calc_scf=True)
-
-    quads = []
-    init_k_KC0 = 0
-    for n1, n2, n3, n4 in zip(n1s, n2s, n3s, n4s):
-        pos1 = nid_pos[n1]
-        pos2 = nid_pos[n2]
-        pos3 = nid_pos[n3]
-        r1 = ncoords[pos1]
-        r2 = ncoords[pos2]
-        r3 = ncoords[pos3]
-        normal = np.cross(r2 - r1, r3 - r2)[2]
-        assert normal > 0
-        quad = Quad4(probe)
-        quad.n1 = n1
-        quad.n2 = n2
-        quad.n3 = n3
-        quad.n4 = n4
-        quad.c1 = DOF*nid_pos[n1]
-        quad.c2 = DOF*nid_pos[n2]
-        quad.c3 = DOF*nid_pos[n3]
-        quad.c4 = DOF*nid_pos[n4]
-        quad.init_k_KC0 = init_k_KC0
-        quad.update_rotation_matrix(ncoords_flatten)
-        quad.update_probe_xe(ncoords_flatten)
-        quad.update_KC0(KC0r, KC0c, KC0v, prop)
-        quads.append(quad)
-        init_k_KC0 += data.KC0_SPARSE_SIZE
-
-    KC0 = coo_matrix((KC0v, (KC0r, KC0c)), shape=(N, N)).tocsc()
-
-    print('elements created')
-
-    bk = np.zeros(N, dtype=bool)
-    check = np.isclose(y, 0.) #| np.isclose(x, a) | np.isclose(y, 0) | np.isclose(y, b)
-    bk[0::DOF] = check
-    bk[1::DOF] = check
-    bk[2::DOF] = check
-    bk[3::DOF] = check
-    bk[4::DOF] = check
-    bk[5::DOF] = check
-
-    bu = ~bk
-
-    #load application
-    fext = np.zeros(N)
-    fmid = 1.
-    check1 = np.isclose(y, b)
-    check2 = np.isclose(y, b) & (np.isclose(x,a) | np.isclose(x,0))
-
-    def uniform_load(dof): 
-        fext[dof::DOF][check1] = fmid/(nx-1)
-        fext[dof::DOF][check2] /= 2
-    
-    # uniform tension, transverse shear
-    uniform_load(1)
-    uniform_load(2)
-
-
-    KC0uu = KC0[bu, :][:, bu]
-    assert np.isclose(fext[bu].sum(), fmid*2)
-
-    uu = spsolve(KC0uu, fext[bu])
-
-    u = np.zeros(N)
-    u[bu] = uu
-
-    x2plot = np.zeros(num_elements)
-    y2plot = np.zeros(num_elements)
-    Nyy2plot = np.zeros(num_elements)
-    Nxy2plot = np.zeros(num_elements)
-    Mxx2plot = np.zeros(num_elements)
-    Mxy2plot = np.zeros(num_elements)
-    Qxz2plot = np.zeros(num_elements)
-    
-    fint = np.zeros(N)
-    for i, quad in enumerate(quads):
-        quad.update_probe_xe(ncoords_flatten)
-        quad.update_probe_ue(u)
-        quad.update_fint(fint, prop)
-        interpols = strains_resultants_quad(probe, prop)
-        x2plot[i] = x[nid_pos[quad.n1]]
-        y2plot[i] = y[nid_pos[quad.n1]]
-        Nyy2plot[i]= interpols["Nyy"](0, 0)
-        Mxx2plot[i]= interpols["Mxx"](0, 0)
-        Mxy2plot[i]= interpols["Mxy"](0, 0)
-        Qxz2plot[i]= interpols["Qxz"](1, -1)
-
-        if 2*a<x2plot[i]<b-1.5*a: #Saint-Venant principle
-            def assert_res_uniform(resultant, expr):
-                ResAround = np.array([interpols[resultant](xi, eta) for xi, eta in [(-1,-1), (1,-1), (-1, 1), (1,1), (0,0)]])
-                assert np.allclose(ResAround, expr, rtol = 1e-2), f"{ResAround}, gt: {expr}, @x {x2plot[i]}"
-            assert_res_uniform("Nyy", fmid/a)
-            #assert_res_uniform("Qyz", fmid/b)
-            #assert_res_uniform("Myy", -(a-x2plot[i])*fmid/b)
-
-        
-
-    print("Stress recovery tests passed.")
-
-    # NOTE adding reaction forces to external force vector
-    Kku = KC0[bk, :][:, bu]
-    fext[bk] = Kku @ u[bu]
-    atol = 1e-5
-    tmp = np.where(np.logical_not(np.isclose(fint, fext, atol=atol)))
-    print(tmp)
-    print(fint[tmp[0]])
-    print(fext[tmp[0]])
-    print((KC0@u)[tmp[0]])
-    assert np.allclose(fint, fext, atol=atol)
-
-    if plot:
-        import matplotlib.pyplot as plt
-
-        ax = plt.subplot(2,2,1)
-        uz = u[2::DOF].reshape(nx,ny).T
-        levels = np.linspace(uz.min(), uz.max(), 20)
-        plt.contourf(xmesh, ymesh, uz, levels=levels)
-        plt.colorbar()
-        ax.set_title("u_z")
-
-        def resultant_plot(i, qty, title):
-            ax = plt.subplot(2,2,i)
-            levels = np.linspace(qty.min(), qty.max(), 20)
-            plt.contourf(x2plot.reshape((nx-1, ny-1)).T, 
-                         y2plot.reshape((nx-1, ny-1)).T, 
-                         qty.reshape((nx-1, ny-1)).T, levels=levels)
-            plt.colorbar()
-            ax.set_title(title)
-
-        resultant_plot(2, Nyy2plot, "Nyy")
-        resultant_plot(3, Qxz2plot, "Qxz")
-        resultant_plot(4, Mxx2plot, "Mxx")
-
-        plt.show()
-
-
-def test_static_plate_quad_point_load_s(plot=False):
-    data = Quad4Data()
-    probe = Quad4Probe()
-    nx = 26
-    ny = 11
-
-    a = 3
-    b = 13
-    h = 0.005 # m
-
-    E = 200e9
-    nu = 0.3
-
-    xtmp = np.linspace(0, a, nx)
-    ytmp = np.linspace(0, b, ny)
-    xmesh, ymesh = np.meshgrid(xtmp, ytmp)
-    ncoords = np.vstack((xmesh.T.flatten(), ymesh.T.flatten(), np.zeros_like(ymesh.T.flatten()))).T
-    x = ncoords[:, 0]
-    y = ncoords[:, 1]
-    z = ncoords[:, 2]
-    ncoords_flatten = ncoords.flatten()
-
-    nids = 1 + np.arange(ncoords.shape[0])
-    nid_pos = dict(zip(nids, np.arange(len(nids))))
-    nids_mesh = nids.reshape(nx, ny)
-    n1s = nids_mesh[:-1, :-1].flatten()
-    n2s = nids_mesh[1:, :-1].flatten()
-    n3s = nids_mesh[1:, 1:].flatten()
-    n4s = nids_mesh[:-1, 1:].flatten()
-
-    num_elements = len(n1s)
-
-    KC0r = np.zeros(data.KC0_SPARSE_SIZE*num_elements, dtype=INT)
-    KC0c = np.zeros(data.KC0_SPARSE_SIZE*num_elements, dtype=INT)
-    KC0v = np.zeros(data.KC0_SPARSE_SIZE*num_elements, dtype=DOUBLE)
-    N = DOF*nx*ny
-
-    prop = isotropic_plate(thickness=h, E=E, nu=nu, calc_scf=True)
-
-    quads = []
-    init_k_KC0 = 0
-    for n1, n2, n3, n4 in zip(n1s, n2s, n3s, n4s):
-        pos1 = nid_pos[n1]
-        pos2 = nid_pos[n2]
-        pos3 = nid_pos[n3]
-        r1 = ncoords[pos1]
-        r2 = ncoords[pos2]
-        r3 = ncoords[pos3]
-        normal = np.cross(r2 - r1, r3 - r2)[2]
-        assert normal > 0
-        quad = Quad4(probe)
-        quad.n1 = n1
-        quad.n2 = n2
-        quad.n3 = n3
-        quad.n4 = n4
-        quad.c1 = DOF*nid_pos[n1]
-        quad.c2 = DOF*nid_pos[n2]
-        quad.c3 = DOF*nid_pos[n3]
-        quad.c4 = DOF*nid_pos[n4]
-        quad.init_k_KC0 = init_k_KC0
-        quad.update_rotation_matrix(ncoords_flatten)
-        quad.update_probe_xe(ncoords_flatten)
-        quad.update_KC0(KC0r, KC0c, KC0v, prop)
-        quads.append(quad)
-        init_k_KC0 += data.KC0_SPARSE_SIZE
-
-    KC0 = coo_matrix((KC0v, (KC0r, KC0c)), shape=(N, N)).tocsc()
-
-    print('elements created')
-
-    bk = np.zeros(N, dtype=bool)
-    check = np.isclose(y, 0.) #| np.isclose(x, a) | np.isclose(y, 0) | np.isclose(y, b)
-    bk[0::DOF] = check
-    bk[1::DOF] = check
-    bk[2::DOF] = check
-    bk[3::DOF] = check
-    bk[4::DOF] = check
-    bk[5::DOF] = check
-
-    bu = ~bk
-
-    #load application
-    fext = np.zeros(N)
-    fmid = 1.
-    check1 = np.isclose(y, b)
-    check2 = np.isclose(y, b) & (np.isclose(x,a) | np.isclose(x,0))
-
-    def uniform_load(dof): 
-        fext[dof::DOF][check1] = fmid/(nx-1)
-        fext[dof::DOF][check2] /= 2
-    
-    # uniform tension, transverse shear
-    uniform_load(0)
-
-
-    KC0uu = KC0[bu, :][:, bu]
-    assert np.isclose(fext[bu].sum(), fmid)
-
-    uu = spsolve(KC0uu, fext[bu])
-
-    u = np.zeros(N)
-    u[bu] = uu
-
-    x2plot = np.zeros(num_elements)
-    y2plot = np.zeros(num_elements)
-    Nyy2plot = np.zeros(num_elements)
-    Nxy2plot = np.zeros(num_elements)
-    Mxx2plot = np.zeros(num_elements)
-    Mxy2plot = np.zeros(num_elements)
-    Qxz2plot = np.zeros(num_elements)
-    
-    fint = np.zeros(N)
-    for i, quad in enumerate(quads):
-        quad.update_probe_xe(ncoords_flatten)
-        quad.update_probe_ue(u)
-        quad.update_fint(fint, prop)
-        interpols = strains_resultants_quad(probe, prop)
-        x2plot[i] = x[nid_pos[quad.n1]]
-        y2plot[i] = y[nid_pos[quad.n1]]
-        Nyy2plot[i] = interpols["Nyy"](0, 0)
-        Mxx2plot[i] = interpols["Mxx"](0, 0)
-        Mxy2plot[i] = interpols["Mxy"](0, 0)
-        Qxz2plot[i] = interpols["Qxz"](1, -1)
-        Nxy2plot[i] = interpols["Nxy"](0, 0)
-
-        if 2*a<x2plot[i]<b-1.5*a: #Saint-Venant principle
-            def assert_res_uniform(resultant, expr):
-                ResAround = np.array([interpols[resultant](xi, eta) for xi, eta in [(-1,-1), (1,-1), (-1, 1), (1,1), (0,0)]])
-                assert np.allclose(ResAround, expr, rtol = 1e-2), f"{ResAround}, gt: {expr}, @x {x2plot[i]}"
-            assert_res_uniform("Nxy", fmid/a)
-            #assert_res_uniform("Qyz", fmid/b)
-            #assert_res_uniform("Myy", -(a-x2plot[i])*fmid/b)
-
-        
-
-    print("Stress recovery tests passed.")
-
-    # NOTE adding reaction forces to external force vector
-    Kku = KC0[bk, :][:, bu]
-    fext[bk] = Kku @ u[bu]
-    atol = 1e-5
-    tmp = np.where(np.logical_not(np.isclose(fint, fext, atol=atol)))
-    print(tmp)
-    print(fint[tmp[0]])
-    print(fext[tmp[0]])
-    print((KC0@u)[tmp[0]])
-    assert np.allclose(fint, fext, atol=atol)
-
-    if plot:
-        import matplotlib.pyplot as plt
-
-        def resultant_plot(i, qty, title):
-            levels = np.linspace(qty.min(), qty.max(), 20)
-            plt.contourf(x2plot.reshape((nx-1, ny-1)).T, 
-                         y2plot.reshape((nx-1, ny-1)).T, 
-                         qty.reshape((nx-1, ny-1)).T, levels=levels)
-            plt.colorbar()
-            plt.title(title)
-
-        resultant_plot(2, Nyy2plot, "Nxy")
+        resultant_plot(1, Nxx, "Nxx")
+        resultant_plot(2, Nyy, "Nyy")
+        resultant_plot(3, Nxy, "Nxy")
+        resultant_plot(4, Qxz, "Qxz")
+        resultant_plot(8, Qyz, "Qyz")
+        resultant_plot(5, Mxx, "Mxx")
+        resultant_plot(6, Myy, "Myy")
+        resultant_plot(7, Mxy, "Mxy")
 
         plt.show()
 
 
 if __name__ == '__main__':
-    test_static_plate_quad_point_load_x(plot=True)
-    test_static_plate_quad_point_load_y(plot=True)
-    test_static_plate_quad_point_load_s(plot=True)
+    test_static_plate_quad_point_load(1, .1, [0,6], plot=True)
+    test_static_plate_quad_point_load(.1, 1, [1,7], plot=True)
+    test_static_plate_quad_point_load(.1, 1, [2,5], plot=True)
