1bf0408efedc11703f0cf6d0f859908221b2bbcb
MarriccZwei <marricc7@gmail.com>
2026-01-02 14:33:11 +0100

    aero load

--- C4/LoadCase.py
+++ C4/LoadCase.py
@@ -8,7 +8,9 @@ import pyfe3d as pf3
 
 class LoadCase():
     def __init__(self, n:float, nlg:float, MTOM:float, N:float, g0:float, 
-                 thrust_total:float, op_point:asb.OperatingPoint, aeroelastic:bool=False):
+                 thrust_total:float, op_point:asb.OperatingPoint, les:nt.NDArray, tes:nt.NDArray,
+                 airfs:ty.List[asb.Airfoil], bres:int=20, cres:int=10, nneighs:int=100,
+                 aeroelastic:bool=False):
         self.n = n #load factor
         self.nlg = nlg #landing gear normal force load factor
         self.N = N #total number of degrees of freedom in the model
@@ -18,6 +20,13 @@ class LoadCase():
         self.op = op_point
         self.MTOM = MTOM
         self.aeroelastic = aeroelastic #do we evaluate flutter for this load case
+
+        self.les = les
+        self.tes = tes
+        self.airfs = airfs
+        self.bres = bres
+        self.cres = cres
+        self.nneighs = nneighs #numer of neighbours
 
         self.A = np.zeros(N) #here be the aerodynamic loads (basically lift)
         self.W = np.zeros(N) #the current value of the model's weight
@@ -45,6 +54,9 @@ class LoadCase():
         :param coords_affect: ncoords-formatted coordinates of the nodes affected
         :type coords_affect: nt.NDArray[np.float64]
         '''
+        _, vlm, _, _ = self._vlm()
+        _, self.A = self._aero2fem(vlm, coords_affected, nid_pos_affected) 
+        print(self.A)
 
     def apply_thrust(self, nid_pos_affected:nt.NDArray[np.int32]):
         '''Applying the thrust at the affected nodes'''
@@ -107,38 +119,40 @@ class LoadCase():
         '''rotates a load from a global coordinate system to the body-centered coordinate system'''
     
     #=====IMPLEMENTATION AERODYNAMIC LOAD==================================
-    def _aero2fem(self, vlm:asb.VortexLatticeMethod, ncoords_s:nt.NDArray[np.float32], ids_s:nt.NDArray[np.int32], N:int, DOF:int,
-             number_of_neighbors=100):
+    def _aero2fem(self, vlm:asb.VortexLatticeMethod, ncoords_s:nt.NDArray[np.float32], ids_s:nt.NDArray[np.int32]):
         """ncoords selected - only selected nodes to which we apply the loads - will have to be tracked externally with ids_s"""
 
         # x, y, z coordinates of each node, NOTE assuming same order for DOFs
         vortex_valid = vlm.vortex_centers[:, 1] > ncoords_s[:, 1].min() # region represented by the FE model
 
         tree = ssp.cKDTree(ncoords_s)
-        d, node_indices = tree.query(vlm.vortex_centers[vortex_valid], k=number_of_neighbors)
+        d, node_indices = tree.query(vlm.vortex_centers[vortex_valid], k=self.nneighs)
 
         power = 2
         weights = (1/d**power)/((1/d**power).sum(axis=1)[:, None])
         assert np.allclose(weights.sum(axis=1), 1)
 
         v = vortex_valid.sum()*3
-        W = np.zeros((N, v))
+        W = np.zeros((self.N, v))
 
         W *= 0
-        for j in range(number_of_neighbors):
+        for j in range(self.nneighs):
             for i, node_index in enumerate(node_indices[:, j]):
                 #Here we cannot use the node_index directly, as we need to match the node indexing in the global mesh
-                W[DOF*ids_s[node_index] + 0, i*3 + 0] += weights[i, j]
-                W[DOF*ids_s[node_index] + 1, i*3 + 1] += weights[i, j]
-                W[DOF*ids_s[node_index] + 2, i*3 + 2] += weights[i, j]
+                W[pf3.DOF*ids_s[node_index] + 0, i*3 + 0] += weights[i, j]
+                W[pf3.DOF*ids_s[node_index] + 1, i*3 + 1] += weights[i, j]
+                W[pf3.DOF*ids_s[node_index] + 2, i*3 + 2] += weights[i, j]
         assert np.allclose(W.sum(axis=0), 1.)
 
         Fv = vlm.forces_geometry[vortex_valid].flatten()
+        #re-scaling the aerodynamic load to match the specified load factor, done due to some load factors not being achievable w\o hlds
+        ratio = self.MTOM*self.g0*self.n*np.cos(np.deg2rad(self.op.alpha))/Fv[2::3].sum()
+        Fv = ratio*Fv
         Fext = W @ Fv #external forces to be applied to the fem model
 
-        assert np.isclose(Fext[0::DOF].sum(), Fv[0::3].sum())
-        assert np.isclose(Fext[1::DOF].sum(), Fv[1::3].sum())
-        assert np.isclose(Fext[2::DOF].sum(), Fv[2::3].sum())
+        assert np.isclose(Fext[0::pf3.DOF].sum(), Fv[0::3].sum())
+        assert np.isclose(Fext[1::pf3.DOF].sum(), Fv[1::3].sum())
+        assert np.isclose(Fext[2::pf3.DOF].sum(), Fv[2::3].sum())
 
         return ss.csc_matrix(W), Fext
 
@@ -159,23 +173,21 @@ class LoadCase():
 
         for j in range(number_of_neighbors):
             for i, node_index in enumerate(node_indices): #we have 3 forces for 2 p vars per node, so 6
-                W_u_to_p[i*6 + 0, DOF*node_index + 0] = weights2[i, j]
-                W_u_to_p[i*6 + 1, DOF*node_index + 1] = weights2[i, j]
-                W_u_to_p[i*6 + 2, DOF*node_index + 2] = weights2[i, j]
-                W_u_to_p[i*6 + 3, DOF*node_index + 0] = weights2[i, j]
-                W_u_to_p[i*6 + 4, DOF*node_index + 1] = weights2[i, j]
-                W_u_to_p[i*6 + 5, DOF*node_index + 2] = weights2[i, j]
+                W_u_to_p[i*6 + 0, DOF*ids_s[node_index] + 0] = weights2[i, j]
+                W_u_to_p[i*6 + 1, DOF*ids_s[node_index] + 1] = weights2[i, j]
+                W_u_to_p[i*6 + 2, DOF*ids_s[node_index] + 2] = weights2[i, j]
+                W_u_to_p[i*6 + 3, DOF*ids_s[node_index] + 0] = weights2[i, j]
+                W_u_to_p[i*6 + 4, DOF*ids_s[node_index] + 1] = weights2[i, j]
+                W_u_to_p[i*6 + 5, DOF*ids_s[node_index] + 2] = weights2[i, j]
             
         return ss.csc_matrix(W_u_to_p)
 
-    def _vlm(self, les:ty.List, tes:ty.List, airfs:ty.List[asb.Airfoil],
-            op:asb.OperatingPoint, bres:int=20, cres:int=10, displs:ty.List[float]=None, return_sol=False):
+    def _vlm(self, displs:ty.List[float]=None, return_sol=False):
         #allowing for initial displacements for flutter. Yet, for static analysis we dont's need displacements
         if displs is None:
-            displs = np.zeros(len(airfs)*2)
-        heaveDir = gcl.Direction3D(0,0,1)
-        cs = [te.x-le.x for te, le in zip(tes, les)] #obtaining the chord list
-        ptles = [heaveDir.step(le, displ) for le, displ in zip(les, displs[:len(airfs)])]
+            displs = np.zeros(len(self.airfs)*2)
+        cs = [self.tes[i,0]-self.les[i,0] for i in range(self.les.shape[0])] #obtaining the chord list
+        ptles = [self.les[i,:]+np.array([0,0,displs[i]]) for i in range(self.les.shape[0])]
 
 
         airplane = asb.Airplane("E9X", xyz_ref=[0,0,0], wings = [
@@ -183,14 +195,14 @@ class LoadCase():
                                         name="E9X_WING",
                                         symmetric=True,
                                         xsecs=[asb.WingXSec(
-                                            xyz_le=[ptle.x, ptle.y, ptle.z],
+                                            xyz_le=[ptle[0], ptle[1], ptle[2]],
                                             chord=c,
                                             twist=twist, 
                                             airfoil=airf
-                                        )for ptle, c, airf, twist in zip(ptles, cs, airfs, displs[len(airfs):])],
+                                        )for ptle, c, airf, twist in zip(ptles, cs, self.airfs, displs[len(self.airfs):])],
                                     )
                                 ])
-        vlm = asb.VortexLatticeMethod(airplane, op, spanwise_resolution=bres, chordwise_resolution=cres)
+        vlm = asb.VortexLatticeMethod(airplane, self.op, spanwise_resolution=self.bres, chordwise_resolution=self.cres)
         sol = vlm.run()
         forces = sol["F_g"]
         moments = sol["M_g"]
