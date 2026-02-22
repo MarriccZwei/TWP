07c2fd2d08b70c8b15bbfd6d3b459014c74b8f2d
MarriccZwei <marricc7@gmail.com>
2026-02-22 09:43:12 +0100

    ElysianWing done

--- C4/Geometry/ElysianWing.py
+++ C4/Geometry/ElysianWing.py
@@ -1,10 +1,12 @@
 import aerosandbox as asb
 import aerosandbox.numpy as np
+import pyvista as pv
 import scipy.interpolate as sip
 
 class ElysianWing():
     def __init__(self, GEOM_SOURCE:dict[str, float | asb.Airfoil], H_per_c_sq:float):
         #0) Overall wing boundaries
+        self.H_per_c_sq = H_per_c_sq
         self.yfus = GEOM_SOURCE["yfus"]
         self.yhn = GEOM_SOURCE["yhn"]
         self.ytip = GEOM_SOURCE["ytip"]
@@ -13,7 +15,7 @@ class ElysianWing():
         delta_x_hn_fus = GEOM_SOURCE["deltaxhn"]
         self._dx_dy = delta_x_hn_fus/(self.yhn-self.yfus)
         self.xtip = self.xle_at_y(self.ytip)
-        self.ztip = self.zle_at_y(self.ztip)
+        self.ztip = self.zle_at_y(self.ytip)
 
         #1) chords
         self.xperc_fore = GEOM_SOURCE["(x/c)_fore"]
@@ -66,10 +68,21 @@ class ElysianWing():
         return self.cr*(1-yfrac)+self.ct*yfrac
     
 
+    def c_reduced_at_y(self, y:float):
+        assert 0.<=y<=self.ytip
+        yfrac = y/self.ytip
+        return self.cr_reduced*(1-yfrac)+self.ct_reduced*yfrac
+
+
     def xle_at_y(self, y:float):
         assert 0.<=y<=self.ytip
         return self._dx_dy*y
     
+
+    def xle_reduced_at_y(self, y:float):
+        assert 0.<=y<=self.ytip
+        return self._dx_dy*y+self.c_reduced_at_y(y)*self.H_per_c_sq/2
+
 
     def zle_at_y(self, y:float):
         assert 0.<=y<=self.ytip
@@ -94,21 +107,93 @@ class ElysianWing():
         coords = yfrac*self._tipXSec_reduced.airfoil.coordinates+(1-yfrac)*self._rootXSec_reduced.airfoil.coordinates
         foil = asb.Airfoil(f"{self._rootXSec_reduced.airfoil.name}->{yfrac}->{self._tipXSec_reduced.airfoil.name}", coords)
         return asb.WingXSec(ptle, c, 0., foil)
-
-
-    def _skin_z(self, xperc:float, y:float, upperSide:int): #xperc wrt the reduced chord
-        assert 0.<=xperc<=1.
-        xsec = self._reduced_xsec_at_y(y)
-        foilcoords = xsec.airfoil.coordinates
-        foilcoords = foilcoords[:, foilcoords[:, 1]*upperSide>=0.]
-        interp = sip.CubicSpline(foilcoords[:, 0], foilcoords[:, 1])
-        return xsec.chord*interp(xperc)
         
 
     def upper_skin_z(self, xperc:float, y:float):
-        return self._skin_z(xperc, y, 1)
+        assert self.xperc_fore<=xperc<=self.xperc_rear
+        xsec = self._reduced_xsec_at_y(y)
+        foilcoords = xsec.airfoil.coordinates
+        foilcoords = foilcoords[(foilcoords[:, 1]>=0.) & (foilcoords[:, 0]<=self.xperc_rear) & (foilcoords[:, 0]>=self.xperc_fore), :]
+        interp = sip.CubicSpline(np.flip(foilcoords[:, 0]), np.flip(foilcoords[:, 1]))
+        return xsec.chord*interp(xperc)+self.zle_at_y(y)
     
     
     def lower_skin_z(self, xperc:float, y:float):
-        return self._skin_z(xperc, y, -1)
+        assert self.xperc_fore<=xperc<=self.xperc_rear
+        xsec = self._reduced_xsec_at_y(y)
+        foilcoords = xsec.airfoil.coordinates
+        foilcoords = foilcoords[(foilcoords[:, 1]<=0.) & (foilcoords[:, 0]<=self.xperc_rear) & (foilcoords[:, 0]>=self.xperc_fore), :]
+        interp = sip.CubicSpline(foilcoords[:, 0], foilcoords[:, 1])
+        return xsec.chord*interp(xperc)+self.zle_at_y(y)
     
+
+    def x_from_xperc(self, xperc:float, y:float):
+        return self.xle_at_y(y)+self.c_at_y(y)*xperc
+    
+
+    def x_from_xperc_reduced(self, xperc:float, y:float):
+        return self.xle_reduced_at_y(y)+self.c_reduced_at_y(y)*xperc
+    
+
+    def plot(self, plotter:pv.Plotter, cres:int=9, bres:int=25):
+        #1) special points -lg and motors
+        ncoords_lgmot_fushn = np.array([
+            [self.xle_at_y(self.yfus), self.yfus, self.zle_at_y(self.yfus)],
+            [self.xlg, self.ylg, self.zlg],
+            [self.xm1, self.ym1, self.zm1],
+            [self.xm2, self.ym2, self.zm2],
+            [self.xm3, self.ym3, self.zm3],
+            [self.xm4, self.ym4, self.zm4],
+            [self.xle_at_y(self.yhn), self.yhn, self.zle_at_y(self.yhn)]
+        ])
+
+        plotter.add_points(
+            ncoords_lgmot_fushn,
+            color="red",
+            point_size=8,
+            render_points_as_spheres=True
+        )
+
+        #2) Skins
+        xpercs = np.linspace(self.xperc_fore, self.xperc_rear, cres)
+        ys = np.linspace(0., self.ytip, bres)
+        xpercmesh, ymesh = np.meshgrid(xpercs, ys)
+        ids = np.arange(bres*cres).reshape(bres, cres)
+
+        cells = list()
+        for i in range(bres-1):
+            for j in range(cres-1):
+                cells.append([4, ids[i, j], ids[i, j+1], ids[i+1, j+1], ids[i+1, j]])
+        cells = np.array(cells).flatten()
+
+        x = np.zeros(bres*cres)
+        y = np.zeros(bres*cres)
+        ztop = np.zeros(bres*cres)
+        zbot = np.zeros(bres*cres)
+        for i in range(bres):
+            for j in range(cres):
+                index = i*cres+j
+                x[index] = self.x_from_xperc_reduced(xpercmesh[i, j], ymesh[i, j]) 
+                y[index] = ymesh[i, j]
+                ztop[index] = self.upper_skin_z(xpercmesh[i, j], ymesh[i, j])
+                zbot[index] = self.lower_skin_z(xpercmesh[i, j], ymesh[i, j])
+        ncoords_top = np.vstack((x, y, ztop)).T
+        ncoords_bot = np.vstack((x, y, zbot)).T
+
+        cell_types = np.full((bres-1)*(cres-1), pv.CellType.QUAD)
+        mesh_top = pv.UnstructuredGrid(cells, cell_types, ncoords_top)
+        mesh_bot = pv.UnstructuredGrid(cells, cell_types, ncoords_bot)
+
+        plotter.add_mesh(
+            mesh_top,
+            show_edges=True,
+            color="lightblue",
+            edge_color="black"
+        )
+
+        plotter.add_mesh(
+            mesh_bot,
+            show_edges=True,
+            color="lightblue",
+            edge_color="black"
+        )
---
+++ test/Geometry/test_ElysianWing.py
@@ -0,0 +1,40 @@
+from ...C4.Geometry.ElysianWing import ElysianWing
+
+import pyvista as pv
+import aerosandbox as asb
+
+def test_ElysianWing():
+    GEOM_SOURCE ={
+        #NOTE: all coordinates in m
+        "yfus":1.602374,
+        "yhn":18.,
+        "ytip":21.,
+        "deltazhn":1.362017,
+        "deltaxhn":1.548961,
+        "(x/c)_fore":.15,
+        "(x/c)_rear":.7,
+        "cr":5.,
+        "ct":2.,
+        "ylg":5.768546,
+        "deltaxlg":.80115,
+        "rlg":.801187,
+        "ym1":4.005935,
+        "ym2":8.091988,
+        "ym3":12.178042,
+        "ym4":16.290801,
+        "deltaxm1":-.801187,
+        "deltaxm2":-.267062,
+        "deltaxm3":.267062,
+        "deltaxm4":.801187,
+        "rootfoil":asb.Airfoil("naca2418"),
+        "tipfoil":asb.Airfoil("naca2410")
+    }
+
+    H_per_c_sq = .01
+    wing = ElysianWing(GEOM_SOURCE, H_per_c_sq)
+    plotter = pv.Plotter()
+    wing.plot(plotter)
+    plotter.show()
+
+if __name__ == "__main__":
+    test_ElysianWing()
