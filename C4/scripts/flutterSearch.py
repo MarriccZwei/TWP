38eca69883bf2032d872b893a44670e6db97b9de
MarriccZwei <marricc7@gmail.com>
2026-03-15 13:04:56 +0100

    Update flutterSearch.py

--- C4/scripts/flutterSearch.py
+++ C4/scripts/flutterSearch.py
@@ -16,7 +16,7 @@ for i, N, Nfoil in zip(range(len(Ns)), Ns, Nfoils):
     t1 = time.time()
     optimiser = Optimiser(mc.DESVARS_INITIAL, [mc.LC_INFO[2]], mc.GEOM_SOURCE, mc.HYPERPARAMS, mc.MASSES, N, mc.MATERIALS, mc.RES, mc.G0, mc.MTOM, Nfoil, mc.LBUCKLSF,
                         mc.BOUNDS)
-    omegan[:, i] = process_aeroelastic_load_case(optimiser.model, optimiser.lcs[0], True,  uc.REFINE_SAVE_PATH+f"{N}\\", mc.RES["kfl"], True)[:nfreq]  
+    omegan[:, i] = process_aeroelastic_load_case(optimiser.model, optimiser.lcs[0], True,  uc.REFINE_SAVE_PATH+f"{i}\\", mc.RES["kfl"], True)[:nfreq]  
     print(f"Processed {Nfoils} in {time.time()-t1} [s], freqs [rad/s]: {omegan[:, i]}\n")  
 
 for j in range(nfreq): 
