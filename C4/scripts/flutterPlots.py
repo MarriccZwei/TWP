42ad95235ba6fe2026fa76b797e28053edd0ab9d
MarriccZwei <marricc7@gmail.com>
2026-03-17 22:32:49 +0100

    Update flutterPlots.py

--- C4/scripts/flutterPlots.py
+++ C4/scripts/flutterPlots.py
@@ -8,21 +8,22 @@ import aerosandbox.numpy as np
 import matplotlib.pyplot as plt
 import scipy.sparse.linalg as ssl
 
-res = 21
-k = 2
-omegan = np.zeros((res, k))
+res = 11
+k = 10
+omegan = np.zeros((res, k), dtype=np.complex64)
 sos = asb.Atmosphere(7000.).speed_of_sound()
-MACHMIN = .6
-MACHMAX = .95
+MACHMIN = .87
+MACHMAX = .99
 VMIN = MACHMIN*sos
 VMAX = MACHMAX*sos
-N = 6
+N = 7
+NAIRFS = 14
 
 machs = np.linspace(MACHMIN, MACHMAX, res)
 lci = mc.LC_INFO[2]
 lci['op'].velocity = VMIN
 lci = [lci]
-optimiser = Optimiser(mc.DESVARS_INITIAL, lci, mc.GEOM_SOURCE, mc.HYPERPARAMS, mc.MASSES, N, mc.MATERIALS, mc.RES, mc.G0, mc.MTOM, mc.NAIRFS, mc.LBUCKLSF,
+optimiser = Optimiser(mc.DESVARS_INITIAL, lci, mc.GEOM_SOURCE, mc.HYPERPARAMS, mc.MASSES, N, mc.MATERIALS, mc.RES, mc.G0, mc.MTOM, NAIRFS, mc.LBUCKLSF,
                         mc.BOUNDS)
 for i, mach in enumerate(machs):
     ratio = (mach/MACHMIN)**2 * np.sqrt((1-MACHMIN**2)/(1-mach**2))
@@ -31,6 +32,7 @@ for i, mach in enumerate(machs):
     print(f"Processing Mach number {mach}...")
     omegan[i, :], _ = ssl.eigs(A=optimiser.model.KC0uu-KAuu, M=optimiser.model.Muu, k=k, sigma=-1., which='LM')
     omegan[i, :] = np.sqrt(omegan[i, :])
+    print(omegan[i, :])
     gc.collect()
 
 print("Preparing plots...")
