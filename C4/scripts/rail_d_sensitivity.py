619c520cf06a450f45e6bb690ead075632f4ae86
MarriccZwei <marricc7@gmail.com>
2026-03-18 16:18:17 +0100

    Update rail_d_sensitivity.py

--- C4/scripts/rail_d_sensitivity.py
+++ C4/scripts/rail_d_sensitivity.py
@@ -13,10 +13,12 @@ resNoBuckl = mc.RES.copy()
 resNoBuckl["klb"] = 0 #exclude linear buckling from iterative analysis as it should be not constraining
 
 rail_ds = [.01,.02,.03, .04, .05]
+hypers = mc.HYPERPARAMS.copy()
 ojectives_report = "=====FINAL REPORT=====\n"
 
 for rail_d in rail_ds:
-    optimiser = Optimiser(desvarsInit, [mc.LC_INFO[0]], mc.GEOM_SOURCE, mc.HYPERPARAMS, mc.MASSES, mc.N, mc.MATERIALS, resNoBuckl, mc.G0, mc.MTOM, mc.NAIRFS, mc.LBUCKLSF,
+    hypers["d"] = rail_d
+    optimiser = Optimiser(desvarsInit, [mc.LC_INFO[0]], mc.GEOM_SOURCE, hypers, mc.MASSES, mc.N, mc.MATERIALS, resNoBuckl, mc.G0, mc.MTOM, mc.NAIRFS, mc.LBUCKLSF,
                         mc.BOUNDS, logEveryNIters=1)
     result = opt.minimize(optimiser.objective, optimiser.desvarvec(), method='COBYLA', constraints=optimiser.constraint(),
                         options={'rhobeg':.2})
