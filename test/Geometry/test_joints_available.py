6a4e5cd62608c05a58538607ce0466011f1f68c1
MarriccZwei <marricc7@gmail.com>
2026-04-28 23:06:25 +0200

    joint safety factor

--- test/Geometry/test_joints_available.py
+++ test/Geometry/test_joints_available.py
@@ -4,12 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def test_Joint():
-    j = Joint(1/4, 1.25, 4080, 7360/2, .5, rho_bolt=7750.) #the smallest bolt from the dataset
+    j = Joint(1/4, 1.25, 4080, 7360/2, bearing_ratio=.5, rho_bolt=7750.) #the smallest bolt from the dataset
     H = .03
     lbf_to_N = 4.44822
 
     #case 1: shear load = 3*allowable in bearing
-    n = j.get_joint_n(0., 7360*.75*lbf_to_N)
+    n = j.get_joint_n(0., 7360*.75*lbf_to_N/2)
     assert n == 4, n
     rj = j.get_joint_dims(n)
     assert np.isclose(rj, .04445), rj
@@ -17,7 +17,7 @@ def test_Joint():
     assert np.isclose(mj, .065381), mj
 
     #case 2: normal load = 2*allowable in tension + .2 allowable in fastener shear
-    n = j.get_joint_n(4080*2.*lbf_to_N, 7360*.1*lbf_to_N)
+    n = j.get_joint_n(4080*2.*lbf_to_N/2, 7360*.1*lbf_to_N/2)
     assert n == 4, n
     rj = j.get_joint_dims(n)
     assert np.isclose(rj, .04445), rj
@@ -32,10 +32,20 @@ def test_Joint():
     mj = j.get_joint_mass(n, H, rj)
     assert np.isclose(mj, .0338374581), mj
 
+    #case 4: 4 bolts in a row
+    j.nrows = 4
+    n = j.get_joint_n(0., 7360*.75*lbf_to_N/2)
+    assert n == 4, n
+    rj = j.get_joint_dims(n)
+    assert np.isclose(rj, .08255), rj
+    mj = j.get_joint_mass(n, H, rj)
+    assert np.isclose(mj, .0663645), mj
+
+
 def test_AvailableJoints():
-    #case 1: increasing the shear load fraction with load magn staying same. expected: increasing sheet thickness
+    #case 1: increasing the shear load fraction with load magn staying same. expected: actually nothing happens because of the way in which the bearing load is defined
     loadfrac = np.linspace(0, 1., 11)*np.pi/2
-    magn = 1e6
+    magn = 1e4
     N = magn*np.cos(loadfrac)
     V = magn*np.sin(loadfrac)
     H = .05
