f3ee30ac7ee1c0ab9b57ff37493a3b39080db092
MarriccZwei <marricc7@gmail.com>
2026-05-08 21:57:25 +0200

    Create rho_foam_sensitivity.py

---
+++ C4/scripts/rho_foam_sensitivity.py
@@ -0,0 +1,32 @@
+from ..Optimiser import Optimiser
+from ..ConfigFiles import mainConfig as mc
+from ..ConfigFiles import userConfig as uc
+
+import scipy.optimize as opt
+import gc
+
+desvarsInit = dict()
+for key in mc.BOUNDS[0].keys(): #centered initial conditions
+    desvarsInit[key]=(mc.BOUNDS[0][key]+mc.BOUNDS[1][key])/2
+
+resNoBuckl = mc.RES.copy()
+resNoBuckl["klb"] = 0 #exclude linear buckling from iterative analysis as it should be not constraining
+
+rho_fs = [.01,.02,.03, .04, .05]
+materials = mc.MATERIALS.copy()
+objectives_report = "=====FINAL REPORT=====\n"
+
+for rho_f in rho_fs:
+    materials['RHO_FOAM'] = rho_f
+    materials['E_FOAM'] = materials["E_ALU"]*(rho_f/materials["RHO_ALU"])**2
+    materials['SF_FOAM'] = 450e6/1.5 * (rho_f/materials["RHO_ALU"])**0.13725 * 10**(-.05803)
+    optimiser = Optimiser(desvarsInit, [mc.LC_INFO[0], mc.LC_INFO[3]], mc.GEOM_SOURCE, mc.HYPERPARAMS, mc.MASSES, mc.N, mc.MATERIALS, resNoBuckl, mc.G0, mc.MTOM, mc.NAIRFS, mc.LBUCKLSF,
+                        mc.BOUNDS, logEveryNIters=1)
+    result = opt.minimize(optimiser.objective, optimiser.desvarvec(), method='COBYLA', constraints=optimiser.constraint(),
+                        options={'rhobeg':.2})
+    desvarsResult = optimiser.desvars_from_vec(result.x)
+    print(f"Converged to: {desvarsResult},\nwith success: {result.success}\nand message: {result.message}")
+    gc.collect()
+    objectives_report += f"For rho foam: {rho_f}, objective: {optimiser.objective(optimiser.desvarvec(desvarsResult))}\nOptimisantion result: {desvarsResult}\n"
+
+print(objectives_report)
