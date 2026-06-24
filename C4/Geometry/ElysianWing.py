e51350a56f0a77965c97c5804a9e9ad39ab4ae95
MarriccZwei <marricc7@gmail.com>
2026-06-24 14:03:15 +0200

    actual elysian spar positions obtained

--- C4/Geometry/ElysianWing.py
+++ C4/Geometry/ElysianWing.py
@@ -6,7 +6,7 @@ import scipy.optimize as op
 import matplotlib.pyplot as plt
 
 class ElysianWing():
-    def __init__(self, GEOM_SOURCE:dict[str, float | asb.Airfoil], H_per_c_sq:float, maxrays:int=15, debug:bool=False, xperc_fore_safe:float=.1, xperc_rear_safe=.9):
+    def __init__(self, GEOM_SOURCE:dict[str, float | asb.Airfoil], H_per_c_sq:float, maxrays:int=15, debug:bool=False, xperc_fore_safe:float=.09, xperc_rear_safe=.9):
         #0) Overall wing boundaries
         self.H_per_c_sq = H_per_c_sq
         self.yfus = GEOM_SOURCE["yfus"]
