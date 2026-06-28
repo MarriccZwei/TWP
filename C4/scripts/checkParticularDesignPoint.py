d54bad2134988ea58d1b5ffa038721728496e4cf
MarriccZwei <marricc7@gmail.com>
2026-06-28 13:19:48 +0200

    check if gc was the issue

--- C4/scripts/checkParticularDesignPoint.py
+++ C4/scripts/checkParticularDesignPoint.py
@@ -12,20 +12,20 @@ res = mc.RES.copy()
 # res["klb"] = 0
 
 desvars_initial = {
-    '(2t/H)_Sq':0.249,
-    '(2t/H)_Pq':0.263,
-    '(2t/H)_Aq':0.096,
-    'W_bb':0.016,
-    'W_mb':0.015,
+    '(2t/H)_Sq':0.2031,
+    '(2t/H)_Pq':0.2112,
+    '(2t/H)_Aq':0.1896,
+    'W_bb':0.0173,
+    'W_mb':0.0178,
     'W_lb':0.030,
-    'ds':0.016,
-    'de':0.013,
-    '(2t/H)_sq':0.168,
-    '(2t/H)_pq':0.146,
-    '(2t/H)_aq':0.077
+    'ds':0.0129,
+    'de':0.0125,
+    '(2t/H)_sq':0.1727,
+    '(2t/H)_pq':0.1697,
+    '(2t/H)_aq':0.1643
 }
 
-optimiser = Optimiser(desvars_initial, mc.LC_INFO, mc.GEOM_SOURCE, hypers, mc.MASSES, mc.N, mc.MATERIALS, res, mc.G0, mc.MTOM, mc.NAIRFS, mc.LBUCKLSF,
+optimiser = Optimiser(desvars_initial, [mc.LC_INFO[3]], mc.GEOM_SOURCE, hypers, mc.MASSES, mc.N, mc.MATERIALS, res, mc.G0, mc.MTOM, mc.NAIRFS, mc.LBUCKLSF,
                       mc.BOUNDS)
 print(optimiser.forward(optimiser.desvarvec(), True, uc.FW_SAVE_PATH))
 print(optimiser.objective(optimiser.desvarvec()))
