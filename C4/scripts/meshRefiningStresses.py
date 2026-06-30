9817bfa24c6a2519fb32f22eb4948b48d52221d3
MarriccZwei <marricc7@gmail.com>
2026-06-30 13:07:56 +0200

    aero mesh resizing

--- C4/scripts/meshRefiningStresses.py
+++ C4/scripts/meshRefiningStresses.py
@@ -14,7 +14,8 @@ res_no_buckl = mc.RES
 res_no_buckl["klb"] = 0
 
 for n in ns:
-    res_no_buckl["nneighs"] = 2 * n
+    res_no_buckl["bres"] = 2 * n
+    res_no_buckl["cres"] = n
     optimiser = Optimiser(mc.DESVARS_INITIAL, lcinfo, mc.GEOM_SOURCE, mc.HYPERPARAMS, mc.MASSES, n, mc.MATERIALS, res_no_buckl, mc.G0, mc.MTOM, mc.NAIRFS, mc.LBUCKLSF,
                         mc.BOUNDS)
     dvv = optimiser.forward(optimiser.desvarvec(), True, uc.REFINE_SAVE_PATH+f"{n}\\")
