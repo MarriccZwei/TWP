d2323fdc5284cc3d6eac712f2ce4a74def14f2e4
MarriccZwei <marricc7@gmail.com>
2026-07-01 22:08:25 +0200

    one zero more

--- C4/scripts/rho_foam_sensitivity.py
+++ C4/scripts/rho_foam_sensitivity.py
@@ -12,7 +12,7 @@ for key in mc.BOUNDS[0].keys(): #centered initial conditions
 resNoBuckl = mc.RES.copy()
 resNoBuckl["klb"] = 0 #exclude linear buckling from iterative analysis as it should be not constraining
 
-rho_fs = [100, 175, 250.2, 325, 400, 475, 550]
+rho_fs = [100, 175, 250.2, 325, 400]
 materials = mc.MATERIALS.copy()
 objectives_report = "=====FINAL REPORT=====\n"
 
