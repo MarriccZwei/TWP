a928bcfd2bb5706bfd254c9420c507f800821dea
MarriccZwei <marricc7@gmail.com>
2026-07-02 21:36:50 +0200

    this commit must be reversed!

--- C4/scripts/meshRefining.py
+++ C4/scripts/meshRefining.py
@@ -2,17 +2,20 @@ from ..Optimiser import Optimiser
 from ..ConfigFiles import mainConfig as mc
 from ..ConfigFiles import userConfig as uc
 import matplotlib.pyplot as plt
+import numpy as np
 
 ns = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]#, 13, 14, 15, 16]
 # beam_stresses = list()
 # quad_stresses = list()
 lms = list()
 
-lcinfo = [mc.LC_INFO[3]]
-lcinfo[0]["Ttot"] = 112800. # [N]
+desvars = {'(2t/H)_Sq': np.float64(0.14779078527131706), '(2t/H)_Pq': np.float64(0.1941012111518347), '(2t/H)_Aq': np.float64(0.17841345464904979), 'W_bb': np.float64(0.01578562454259829), 'W_mb': np.float64(0.018572223880988704), 'W_lb': np.float64(0.01574421166844625), 'ds': np.float64(0.015763858666424515), 'de': np.float64(0.01780049033655861), '(2t/H)_sq': np.float64(0.16046288064007755), '(2t/H)_pq': np.float64(0.10110644736027476), '(2t/H)_aq': np.float64(0.12659052092093015)}
+
+
+lcinfo = [mc.LC_INFO[0]]
 
 for n in ns:
-    optimiser = Optimiser(mc.DESVARS_INITIAL, lcinfo, mc.GEOM_SOURCE, mc.HYPERPARAMS, mc.MASSES, n, mc.MATERIALS, mc.RES, mc.G0, mc.MTOM, mc.NAIRFS, mc.LBUCKLSF,
+    optimiser = Optimiser(desvars, lcinfo, mc.GEOM_SOURCE, mc.HYPERPARAMS, mc.MASSES, n, mc.MATERIALS, mc.RES, mc.G0, mc.MTOM, mc.NAIRFS, mc.LBUCKLSF,
                         mc.BOUNDS)
     dvv = optimiser.forward(optimiser.desvarvec(), True, uc.REFINE_SAVE_PATH+f"{n}\\")
     # quad_stresses.append(dvv[0])
