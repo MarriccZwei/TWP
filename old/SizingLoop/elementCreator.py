a8622e0cf725183e21fadc0a104755ae6ddc55d3
MarriccZwei <marricc7@gmail.com>
2026-07-06 13:35:21 +0200

    clean old things

--- old/SizingLoop/elementCreator.py
+++
@@ -1,8 +0,0 @@
-import componentClasses as ccl
-import typing as ty
-import geometricClasses as gcl
-
-def eles(nodes:ty.List[ty.List[gcl.Point3D]], components:ty.List[ccl.Component])->ty.List:
-    '''This modules creates elements attached to appropriate mesh nodes, based on co-indexed lists of components and parts of mesh corresponding to components of the same index.
-    Also, creates mass elements for eles and spring elements for joints'''
-    raise NotImplementedError
