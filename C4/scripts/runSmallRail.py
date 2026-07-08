dc170014a050e7b8243af43f3afd1e0e6dbe9ad0
MarriccZwei <marricc7@gmail.com>
2026-07-09 00:00:08 +0200

    Create runSmallRail.py

---
+++ C4/scripts/runSmallRail.py
@@ -0,0 +1,29 @@
+from ..Optimiser import Optimiser
+from ..ConfigFiles import mainConfig as mc
+from ..ConfigFiles import userConfig as uc
+
+import scipy.optimize as opt
+import gc
+
+desvarsInit = dict()
+for key in mc.BOUNDS[0].keys(): #centered initial conditions
+    desvarsInit[key]=(mc.BOUNDS[0][key]+mc.BOUNDS[1][key])/2
+
+hypers = mc.HYPERPARAMS.copy()
+hypers["d"] = 0.01
+
+resNoBuckl = mc.RES.copy()
+resNoBuckl["klb"] = 0 #exclude linear buckling from iterative analysis as it should be not constraining
+
+optimiser = Optimiser(desvarsInit, [mc.LC_INFO[0], mc.LC_INFO[1], mc.LC_INFO[3]], mc.GEOM_SOURCE, hypers, mc.MASSES, mc.N, mc.MATERIALS, resNoBuckl, mc.G0, mc.MTOM, mc.NAIRFS, mc.LBUCKLSF,
+                      mc.BOUNDS, logEveryNIters=1, loadCasesJoint=mc.LC_INFO)
+result = opt.minimize(optimiser.objective, optimiser.desvarvec(), method='COBYLA', constraints=optimiser.constraint(),
+                      options={'rhobeg':.2})
+desvarsResult = optimiser.desvars_from_vec(result.x)
+print(f"Converged to: {desvarsResult},\nwith success: {result.success}\nand message: {result.message}")
+gc.collect()
+
+#checking constraints for the converged design
+verifier = Optimiser(desvarsResult, mc.LC_INFO, mc.GEOM_SOURCE, mc.HYPERPARAMS, mc.MASSES, mc.N, mc.MATERIALS, mc.RES, mc.G0, mc.MTOM, mc.NAIRFS, mc.LBUCKLSF,
+                      mc.BOUNDS)
+print(verifier.forward(verifier.desvarvec(), True, uc.FW_SAVE_PATH))
